03f104b89e66016688ae60680c0de1e05cd61242
Stoyan Stoyanov <stoianov.stoian@gmail.com>
2022-11-28 21:10:13 +0200

    First code commit

---
+++ src/test/java/test.java
@@ -0,0 +1,53 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+
+public class test {
+    public WebDriver driver;
+
+    @BeforeMethod
+    public void setUp(){
+        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("--headless");
+        options.addArguments("--window-size=1920x1080");
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        Actions action = new Actions(driver);
+    }
+
+    @AfterMethod
+    public void close(){
+        driver.close();
+    }
+
+    @Test
+    public void testCheckboxex() throws InterruptedException {
+        driver.get("https://the-internet.herokuapp.com/checkboxes");
+        By checkboxes = By.cssSelector("#checkboxes input");
+        WebElement checkboxOne = driver.findElements(checkboxes).get(0);
+        WebElement checkboxTwo = driver.findElements(checkboxes).get(1);
+
+        Assert.assertFalse(checkboxOne.isSelected());
+        Assert.assertTrue(checkboxTwo.isSelected());
+
+        Thread.sleep(5000);
+
+        checkboxOne.click();
+        checkboxTwo.click();
+
+        Assert.assertTrue(checkboxOne.isSelected());
+        Assert.assertFalse(checkboxTwo.isSelected());
+
+        Thread.sleep(5000);
+    }
+}
